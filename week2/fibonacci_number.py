4da5b588a68dfcec8a2d4584dc548a545da69247
Raviteja <rpodila@qti.qualcomm.com>
2023-05-25 21:10:06 +0000

    Week2 assignment p1

--- week2/fibonacci_number.py
+++ week2/fibonacci_number.py
@@ -2,7 +2,11 @@
 def fibonacci_number(n):
     if n <= 1:
         return n
-    return fibonacci_number(n-2) + fibonacci_number(n-1)
+    previous = 0
+    current = 1
+    for _ in range(n-1):
+        previous, current = current, previous + current
+    return current
 
 if __name__ == "__main__":
     input_n = int(input())
