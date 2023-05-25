4aa54de56dbaf5e6a5eefe84316a1b3c2b9add3b
Raviteja <rpodila@qti.qualcomm.com>
2023-05-25 16:49:30 +0000

    Algorithm updated to reduce run time

--- week2/huge_fibonacci_number.py
+++ week2/huge_fibonacci_number.py
@@ -1,14 +1,14 @@
-
-def fibonacci_number(n):
+def fibonacci_number(n, m):
     if n <= 1:
         return n
     previous = 0
     current = 1
     for _ in range(n-1):
         previous, current = current, previous + current
-    last_digit = current % 10
-    return last_digit
+    return current % m
+
 
 if __name__ == "__main__":
-    input_n = int(input())
-    print(fibonacci_number(input_n))
+    input_i = map(int, input().split())
+    n, m = input_i
+    print(fibonacci_number(n, m))
