a859d9481d7849b47735c339bcf48ee5a7347eb8
Raviteja <ravitejaroyal@gmail.com>
2023-06-16 13:54:28 -0500

    updated fibonacci algorithms to improve execution time

--- week2/partial_sum_last_digit_fibonacci_number.py
+++ week2/partial_sum_last_digit_fibonacci_number.py
@@ -1,20 +1,14 @@
 def fibonacci_number(m, n):
-    n = n % 60
     if n <= 1:
         return n
     previous = 0
     current = 1
-    if m > 2:
-        sum = 0
-    else:
-        sum = 1
+    sum = 0
     for i in range(n-1):
-        tmp = previous
-        previous =  current
-        current = (tmp + current) % 10
+        previous, current = current, previous + current
         if i >= m - 2:
-            sum = (sum + current) % 10
-    return sum
+            sum += current
+    return sum % 10
 
 if __name__ == "__main__":
     m, n = map(int, input().split())
--- week2/sum_of_squares_last_digit_fibonacci_number.py
+++ week2/sum_of_squares_last_digit_fibonacci_number.py
@@ -1,13 +1,16 @@
 def fibonacci_number(n):
+    n = n % 60
     if n <= 1:
         return n
     previous = 0
     current = 1
     sum = 1
     for _ in range(n-1):
-        previous, current = current, previous + current
-        sum += current * current
-    return sum % 10
+        tmp = previous
+        previous =  current
+        current = (tmp + current) % 10
+        sum  = (sum + (current * current)) % 10
+    return sum
 
 if __name__ == "__main__":
     input_n = int(input())
