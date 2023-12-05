c6c2b9f7c68a8a7bdf0e6aff89c716d9d176a6bf
Raviteja <ravitejaroyal@gmail.com>
2023-12-05 10:24:53 -0600

    Added new logic for covering segments

--- week3_greedy_algorithms/covering_segments.py
+++ week3_greedy_algorithms/covering_segments.py
@@ -17,6 +17,7 @@ def optimal_points(segments):
 
 
 if __name__ == '__main__':
+    input = stdin.read()
     n, *data = map(int, input().split())
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
